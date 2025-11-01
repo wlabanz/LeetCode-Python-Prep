a160f9a23c22015b2f74179c0aceb088e931eed3
wlabanz <148989689+wlabanz@users.noreply.github.com>
2025-11-01 16:23:47 -0400

    Create Implement-Trie.py

---
+++ medium/Implement-Trie.py
@@ -0,0 +1,14 @@
+class PrefixTree:
+
+    def __init__(self):
+        
+
+    def insert(self, word: str) -> None:
+
+
+    def search(self, word: str) -> bool:
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        
+        
